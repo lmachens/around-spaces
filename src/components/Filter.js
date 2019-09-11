0840331f1195ad29269935f9c342d3f10ba67921
lmachens <leon.machens@googlemail.com>
2019-09-11 12:09:48 +0200

    add placeholder components

---
+++ src/components/Filter.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Filter() {
+  return <div>Filter</div>;
+}
+
+export default Filter;
---
+++ src/components/RestaurantList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Restaurant from "./Restaurant";
+
+function RestaurantList() {
+  return (
+    <div>
+      RestaurantList <Restaurant />
+    </div>
+  );
+}
+
+export default RestaurantList;
