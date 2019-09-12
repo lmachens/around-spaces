a229bc852749e1cdbb28c0d703a3045e27104c9f
lmachens <leon.machens@googlemail.com>
2019-09-12 15:58:02 +0200

    show all fields in restaurant card

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,7 +1,31 @@
 import React from "react";
 
 function Restaurant(props) {
-  return <div className="card">{props.restaurant.title}</div>;
+  return (
+    <div className="card">
+      {props.restaurant.title}
+      <img
+        src={props.restaurant.imgSrc}
+        className="card__img"
+        alt={props.restaurant.title}
+      />
+      <p>{props.restaurant.description}</p>
+      <p>
+        Distance: {props.restaurant.distance} minutes
+        <br />
+        Rating: {props.restaurant.rating} stars
+        <br />
+        Categories:{" "}
+        {props.restaurant.categories.map(category => {
+          return (
+            <span className="card__category" key={category}>
+              {category}
+            </span>
+          );
+        })}
+      </p>
+    </div>
+  );
 }
 
 export default Restaurant;
