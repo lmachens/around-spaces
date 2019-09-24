5fa33f35ca317c6a7e6ef9bf0f48cb4a3d1471b6
lmachens <leon.machens@googlemail.com>
2019-09-24 11:53:21 +0200

    add filters to search/queryParams

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,18 +14,26 @@ const Main = styled.main`
   padding-bottom: 25px;
 `;
 
-export default function Home() {
-  const [filters, setFilters] = React.useState({});
+export default function Home({ history, location }) {
+  const params = new URLSearchParams(location.search);
+  const [filters, setFilters] = React.useState({
+    category: params.get("category") || "",
+    distance: params.get("distance") || "",
+    rating: params.get("rating") || ""
+  });
 
   function handleFilterChange(name, value) {
     // Create a copy of filters object
     const newFilters = { ...filters }; // or Object.assign({}, filters);
+    params.delete(name);
     if (value) {
       newFilters[name] = value;
+      params.append(name, value);
     } else {
       delete newFilters[name];
     }
     setFilters(newFilters);
+    history.push(`${location.pathname}?${params.toString()}`);
   }
 
   return (
