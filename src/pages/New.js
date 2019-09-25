6b1a4b7f5829889d1d22f294cd48feeaf0f9ba2b
Hanna Frauenkron <h.frauenkron@mail.de>
2019-09-25 16:35:04 +0200

    add state getInput

--- src/pages/New.js
+++ src/pages/New.js
@@ -22,10 +22,10 @@ const AddCircleIcon = styled(Add)`
 
 export default function New() {
   function handleAccept() {
-    postRestaurant({
+    return postRestaurant({
       imgSrc:
         "https://static.lieferando.de/images/restaurants/de/OPROO0P/logo_465x320.png",
-      title: "Pasta Pronto",
+      title: `${Input}`,
       categories: ["pizza", "salad", "pasta"],
       distance: 6,
       rating: 3.1,
@@ -33,12 +33,18 @@ export default function New() {
     });
   }
 
+  const [Input, getInput] = React.useState("");
+
   return (
     <Modal hideBackdrop onAccept={handleAccept}>
       <AddCircleIcon />
       <ModalTitle>Add Restaurant</ModalTitle>
       <ModalSection>Name</ModalSection>
-      <TextInput placeholder="Enter restaurant name" />
+      <TextInput
+        placeholder="Enter restaurant name"
+        value={Input}
+        onChange={event => getInput(event.target.value)}
+      />
       <ModalSection>Categories</ModalSection>
       <Flex>
         {getCategoryOptions().map(option => (
