5eef06f4f92763bd92495f1adcd9216b0e464772
lmachens <leon.machens@googlemail.com>
2019-09-26 14:52:17 +0200

    post analytics on filter change in effect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -27,22 +27,31 @@ export default function Home({ history, location, toggleTheme }) {
     rating: params.get("rating") || ""
   });
 
+  React.useEffect(() => {
+    postAnalytics({
+      selectedFilters: filters,
+      time: Date.now()
+    });
+  }, [location.search]);
+
+  React.useEffect(() => {
+    const newParams = new URLSearchParams();
+    Object.entries(filters).forEach(([key, value]) => {
+      if (value) {
+        newParams.append(key, value);
+      }
+    });
+    history.push(`${location.pathname}?${newParams.toString()}`);
+  }, [filters]);
+
   function handleFilterChange(name, value) {
     const newFilters = { ...filters };
-    params.delete(name);
     if (value) {
       newFilters[name] = value;
-      params.append(name, value);
     } else {
       delete newFilters[name];
     }
     setFilters(newFilters);
-    history.push(`${location.pathname}?${params.toString()}`);
-
-    postAnalytics({
-      selectedFilters: newFilters,
-      time: Date.now()
-    });
   }
 
   return (
